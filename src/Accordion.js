542b6b11679d1dd516a629daa219ecd073d58990
unknown <jirka.brtnik@post.cz>
2021-09-01 10:37:15 +0200

    added functions

---
+++ src/Accordion.js
@@ -0,0 +1,3 @@
+import React from "react";
+const Accordion = (props) => {};
+export default Accordion;
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+items = {
+  0: {
+    heading: "I like boobs",
+    text: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ",
+  },
+  1: {
+    heading: "I like boobs",
+    text: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ",
+  },
+  2: {
+    heading: "I like boobs",
+    text: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ",
+  },
+  3: {
+    heading: "I like boobs",
+    text: "lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem ",
+  },
+};
+const App = (props) => {
+  <Accordion items={items} />;
+};
+export default App;
