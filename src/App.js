cdf15b24b60fd7f5e3ad04dc222dc5b9c27b3389
unknown <jirka.brtnik@post.cz>
2021-09-16 14:08:46 +0200

    not great approach to navigation'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Header from "./Header";
 import Accordion from "./Accordion";
 import CounterExample from "./CounterExample";
 import SearchBar from "./Api";
 import Dropdown from "./Dropdown";
 import Translate from "./Translate";
+import Route from "./Route";
 
 // Must be array not
 const items = [
@@ -32,30 +34,52 @@ const options = [
     value: "green",
   },
 ];
-const showAccordion = () => {
-  if (window.location.pathname === "/") {
-    return <Accordion items={items} />;
-  }
-};
-const showList = () => {
-  if (window.location.pathname === "/list") {
-  }
-};
+// const showAccordion = () => {
+//   if (window.location.pathname === "/") {
+//     return <Accordion items={items} />;
+//   }
+// };
+// const showList = () => {
+//   if (window.location.pathname === "/list") {
+//   }
+// };
 
-const showTranslate = () => {
-  if (window.location.pathname === "/translate") {
-  }
-};
+// const showTranslate = () => {
+//   if (window.location.pathname === "/translate") {
+//     return <Translate />;
+//   }
+// };
 
-const showList = () => {
-  if (window.location.pathname === "/list") {
-  }
-};
+// const showDropdown = () => {
+//   if (window.location.pathname === "/dropdown") {
+//     return (
+//       <Dropdown options={options} label="I like trains" selected={selected} />
+//     );
+//   }
+// };
 const App = (props) => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div className="ui text container">
+      <Header />
       {/* <Translate /> */}
-      {showAccordion()}
+      <Route path="/">
+        <SearchBar />
+      </Route>
+      <Route path="/acc">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
       {/* <Fetcher /> */}
     </div>
   );
